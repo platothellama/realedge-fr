abe4db23c2018e493b18aacbd7401930fe19ad4b
platothellama <platothellama@gmail.com>
2026-09-13 23:36:53 +0300

    Update feature.service.ts

--- src/app/services/feature/feature.service.ts
+++ src/app/services/feature/feature.service.ts
@@ -13,7 +13,7 @@ export interface FeatureFlags {
   providedIn: 'root'
 })
 export class FeatureService {
-  private apiUrl = 'https://realedge-frontend-production.up.railway.app/api/features';
+  private apiUrl = 'https://realedge-frontend-t9d6.vercel.app/api/features';
   
   features = signal<FeatureFlags>({});
 
