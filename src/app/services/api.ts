1d093204bec659c3df19fe2bf70e1a9e5d39530d
platothellama <platothellama@gmail.com>
2026-09-13 23:36:02 +0300

    Update api.ts

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'https://realedge-frontend-production.up.railway.app/api';
+  private apiUrl = 'https://realedge-frontend-t9d6.vercel.app/api';
 
   constructor(private http: HttpClient) {}
 
